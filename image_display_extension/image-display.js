a44f2377e58a574a347c3ad094f7c4d1ed407c92
penduram203 <136647758+penduram203@users.noreply.github.com>
2025-07-03 13:23:25 +0900

    Update image-display.js

--- image_display_extension/image-display.js
+++ image_display_extension/image-display.js
@@ -455,7 +455,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // ローカルファイルを取得
         try {
-            const response = await fetch(`scripts/extensions/image_display_extension/character_image_mapping/${characterName}.json`);
+            const response = await fetch(`addchara/${characterName}.json`);
             if (response.ok) {
                 const jsonData = await response.json();
                 
